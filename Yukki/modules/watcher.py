c3b239ed13b984cb64584a61a9894f52a1f5dc35
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-29 22:12:55 +0530

    Update watcher.py

--- Yukki/modules/watcher.py
+++ Yukki/modules/watcher.py
@@ -33,7 +33,7 @@ async def welcome(_, message: Message):
     vedia = random.choice(MEDIA)
     notes = random.choice(TEXT)
     await message.reply_video(vedia, caption=notes)
-    await put_cleanmode(message.chat.id, send.message_id)
+    await put_cleanmode(message.chat.id)
     
 
 
