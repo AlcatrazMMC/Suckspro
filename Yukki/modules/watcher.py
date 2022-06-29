f2c95947295b01a63664556111831821cea4fcfa
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-29 21:43:51 +0530

    Update watcher.py

--- Yukki/modules/watcher.py
+++ Yukki/modules/watcher.py
@@ -29,7 +29,7 @@ WLMTEXT = [
 
 @app.on_message(filters.new_chat_members, group=welcome_group)
 async def welcome(_, message: Message):
-    gone = 
+    gone = message.reply_video(video={random.choice(WELCOME)}, caption={random.choice(WLMTEXT)})
     await message.reply_video(video={random.choice(WELCOME)}, caption={random.choice(WLMTEXT)})
     if message.chat.id in last:
         old_id = last[message.chat.id]
