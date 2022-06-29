58b79c6c073dca045caf25e63974e455d2ea4c8a
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-06-29 17:59:45 +0530

    Update watcher.py

--- Yukki/modules/watcher.py
+++ Yukki/modules/watcher.py
@@ -31,14 +31,15 @@ LAST = []
 async def welcome(_, message: Message):
     image = random.choice(WELCOME)
     wtext = random.choice(WLMTEXT)
+    await message.reply_video(video=image, caption=wtext)
     if message.chat.id in last:
-    old_id = last[message.chat.id]
+        old_id = last[message.chat.id]
     try:
-    await app.delete_messages(message.chat.id , old_id)
+        await app.delete_messages(message.chat.id , old_id)
     except:
         pass
     last[message.chat.id] = gone.message_id
-    await message.reply_video(video=image, caption=wtext)
+    
 
 
 
